3204cc70fbc6fbecd907757f3206ae466cd69a9c
Steve Tuckner <steve@mrets.org>
2024-11-24 16:20:43 -0600

    Got bi-directional date name / date working

--- app/todos_presenter.rb
+++ app/todos_presenter.rb
@@ -3,12 +3,15 @@ require_relative 'todo_model'
 require_relative 'todos_model'
 
 class DateProxy
-  def initialize(date, presenter, hash_sym, &update_date_block)
+  def initialize(date, presenter, hash_sym)
     @date = date
     @presenter = presenter
     @hash_sym = hash_sym
-    @update_date_block = update_date_block
     update_date_hash(@date)
+  end
+
+  def on_change(&update_date_block)
+    @update_date_block = update_date_block
   end
 
   def update_date(date)
@@ -35,7 +38,7 @@ class DateProxy
 
   def after_due_date_write(date_hash)
     @date = to_date(date_hash)
-    @update_date_block.call(@date)
+    @update_date_block.call(@date) if @update_date_block
   end
 
   def to_date(date_hash)
@@ -49,10 +52,23 @@ end
 
 class DateNameSelect
   attr_accessor :selected_index_value, :date_names_array
-  def initialize(&update_date_block)
+  def initialize
     @selected_index_value = 0
     @date_names_array = ['Today', 'Tomorrow', 'Custom']
+  end
+
+  def on_change(&update_date_block)
     @update_date_block = update_date_block
+  end
+
+  def update_date(date)
+    if date == Time.now.to_date
+      self.selected_index_value = 0
+    elsif date == Time.now.to_date + 1
+      self.selected_index_value = 1
+    else
+      self.selected_index_value = 2
+    end
   end
 
   def items_binding_array
@@ -69,6 +85,13 @@ class DateNameSelect
 
   def after_selected_date_name_index_write(index)
     puts "@@selected_index_value; #{@selected_index_value}"
+    if @update_date_block
+      if index == 0
+        @update_date_block.call(Time.now.to_date)
+      elsif index == 1
+        @update_date_block.call(Time.now.to_date + 1)
+      end
+    end
   end
 end
 
@@ -82,12 +105,17 @@ class TodosPresenter
 
   def initialize(start_date)
     @new_todo = TodoModel.new('', due_date: start_date)
-    @due_date_proxy = DateProxy.new(start_date, self, :new_due_date_hash) do |date|
+    @due_date_proxy = DateProxy.new(start_date, self, :new_due_date_hash)
+    @date_name_select = DateNameSelect.new
+
+    @due_date_proxy.on_change do |date|
       @new_todo.due_date = date
+      @date_name_select.update_date(date)
     end
-    @date_name_select = DateNameSelect.new do |date|
+    @date_name_select.on_change do |date|
       @due_date_proxy.update_date(date)
     end
+
     load_todos
   end
 
